65659e07cf34624b9af1599d4f5eeccb8c48c3b4
KAWAZOME Ichiro <ichiro_k@ca2.so-net.ne.jp>
2014-03-13 23:13:50 +0900

    REGISTER_ACCESS_DECODER を、より速い回路が生成されるようにリファクタリングした.

--- src/components/components.vhd
+++ src/components/components.vhd
@@ -2,7 +2,7 @@
 --!     @file    components.vhd                                                  --
 --!     @brief   PIPEWORK COMPONENT LIBRARY DESCRIPTION                          --
 --!     @version 1.5.5                                                           --
---!     @date    2014/03/08                                                      --
+--!     @date    2014/03/13                                                      --
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>                     --
 -----------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------
