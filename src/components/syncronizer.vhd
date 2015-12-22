7d2388eaf058e971de2a68a73073706e2a0d6ece
KAWAZOME Ichiro <ichiro_k@ca2.so-net.ne.jp>
2015-12-22 10:12:06 +0900

    SYNCRONIZER の非同期時の入力FFの段数を２段に変更した.

--- src/components/syncronizer.vhd
+++ src/components/syncronizer.vhd
@@ -2,12 +2,12 @@
 --!     @file    syncronizer.vhd
 --!     @brief   SYNCRONIZER MODULE :
 --!              異なるクロックで動作するパスを継ぐアダプタのクロック同期化モジュール.
---!     @version 1.5.0
---!     @date    2013/4/2
+--!     @version 1.5.9
+--!     @date    2015/12/22
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
---      Copyright (C) 2012,2013 Ichiro Kawazome
+--      Copyright (C) 2012-2015 Ichiro Kawazome
 --      All rights reserved.
 --
 --      Redistribution and use in source and binary forms, with or without
@@ -94,7 +94,7 @@ entity  SYNCRONIZER is
                       --! * FFで叩くのはメタステーブルの発生による誤動作を防ぐため.
                       --!   メタステーブルの意味が分からない人は、この変数を変更す
                       --!   るのはやめたほうがよい。
-                      integer range 0 to 1 := 1;
+                      integer range 0 to 2 := 2;
         O_CLK_FLOP  : --! @brief OUTPUT CLOCK FLOPPING :
                       --! 入力側のクロック(I_CLK)と出力側のクロック(O_CLK)が非同期
                       --! の場合に、入力側のFFからの制御信号を出力側のFFで叩く段数
@@ -102,7 +102,7 @@ entity  SYNCRONIZER is
                       --! * FFで叩くのはメタステーブルの発生による誤動作を防ぐため.
                       --!   メタステーブルの意味が分からない人は、この変数を変更す
                       --!   るのはやめたほうがよい.
-                      integer range 0 to 1 := 1;
+                      integer range 0 to 2 := 2;
         I_CLK_FALL  : --! @brief USE INPUT CLOCK FALL :
                       --! 入力側のクロック(I_CLK)と出力側のクロック(O_CLK)が非同期
                       --! の場合に、入力側のクロック(I_CLK)の立ち下がりを使うかどう
