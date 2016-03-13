656a5dfffdc00a70af0abdbb9e968614b3a6cdc9
KAWAZOME Ichiro <ichiro_k@ca2.so-net.ne.jp>
2016-03-13 17:52:44 +0900

    SDPRAM(ALTERA_AUTO_SELECT)のバイトイネーブル信号の生成がすべてALL1になってしまう不具合を修正.

--- src/components/components.vhd
+++ src/components/components.vhd
@@ -2,7 +2,7 @@
 --!     @file    components.vhd                                                  --
 --!     @brief   PIPEWORK COMPONENT LIBRARY DESCRIPTION                          --
 --!     @version 1.5.9                                                           --
---!     @date    2016/01/05                                                      --
+--!     @date    2016/03/13                                                      --
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>                     --
 -----------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------
