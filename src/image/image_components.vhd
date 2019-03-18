c37ae293e4380b4127bc01c2ed5bf0c8a134a458
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2019-03-18 15:24:55 +0900

    [fix] invalid O_VALID or I_READY assert timing in IMAGE_STREAM_GENERATOR

--- src/image/image_components.vhd
+++ src/image/image_components.vhd
@@ -2,7 +2,7 @@
 --!     @file    image_components.vhd                                            --
 --!     @brief   PIPEWORK IMAGE COMPONENTS LIBRARY DESCRIPTION                   --
 --!     @version 1.8.0                                                           --
---!     @date    2019/03/07                                                      --
+--!     @date    2019/03/18                                                      --
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>                     --
 -----------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------
