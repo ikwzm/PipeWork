1758bae6c624de30e1f634da8a4ef69eff7cc22c
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2023-12-18 14:57:30 +0900

    [update] PUMP_STREAM_OUTLET_CONTROLLER to support POOL_INTAKE_PORT new version.

--- src/pump/pump_stream_outlet_controller.vhd
+++ src/pump/pump_stream_outlet_controller.vhd
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------------------
 --!     @file    pump_stream_outlet_controller.vhd
 --!     @brief   PUMP STREAM OUTLET CONTROLLER
---!     @version 1.9.0
---!     @date    2023/12/15
+--!     @version 2.0.0
+--!     @date    2023/12/17
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
