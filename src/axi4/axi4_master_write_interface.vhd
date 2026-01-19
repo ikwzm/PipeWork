2c16f1166407c22439a46a824706a7ba2a3230c0
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2026-01-19 13:16:58 +0900

    [update] AXI4_MASTER_WRITE_INTERFACE following updates to AXI4_DATA_OUTLET_PORT

--- src/axi4/axi4_master_write_interface.vhd
+++ src/axi4/axi4_master_write_interface.vhd
@@ -1,12 +1,12 @@
 -----------------------------------------------------------------------------------
 --!     @file    axi4_master_write_interface.vhd
 --!     @brief   AXI4 Master Write Interface
---!     @version 2.2.0
---!     @date    2024/4/7
+--!     @version 2.6.0
+--!     @date    2026/1/19
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
---      Copyright (C) 2012-2024 Ichiro Kawazome
+--      Copyright (C) 2012-2026 Ichiro Kawazome
 --      All rights reserved.
 --
 --      Redistribution and use in source and binary forms, with or without
