8a7f9fd509e37a0e708001d6292937800b1c373c
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2026-01-19 13:17:58 +0900

    [update] AXI4_SLAVE_READ_INTERFACE following updates to AXI4_DATA_OUTLET_PORT

--- src/axi4/axi4_slave_read_interface.vhd
+++ src/axi4/axi4_slave_read_interface.vhd
@@ -1,12 +1,12 @@
 -----------------------------------------------------------------------------------
 --!     @file    axi4_slave_read_interface.vhd
 --!     @brief   AXI4 Slave Read Interface
---!     @version 2.0.0
---!     @date    2024/2/19
+--!     @version 2.6.0
+--!     @date    2026/1/19
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
---      Copyright (C) 2012-2024 Ichiro Kawazome
+--      Copyright (C) 2012-2026 Ichiro Kawazome
 --      All rights reserved.
 --
 --      Redistribution and use in source and binary forms, with or without
